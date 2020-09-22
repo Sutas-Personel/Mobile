e708b34734217f388bff9a711c293abb3961d44f
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-22 23:06:52 +0300

    added const colors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AllColors {
+  static const Color MAIN_GREEN = Color(0xff006e3a);
+  static const Color NASTY_GREEN = Color(0xff70b243);
+  static const Color MAIN_LIGHT_GREEN = Color(0xff56a024);
+  static const Color BLUE_GRAY = Color(0xff9da1b9);
+  static const Color ORANGE_YELLOW = Color(0xffffa502);
+  static const Color INSTAGRAM_PURPLE = Color(0xffab1a9b);
+  static const Color TWITTER_BLUE = Color(0xff1da1f2);
+  static const Color WHATSAPP_GREEN = Color(0xff05cd51);
+  static const Color LINKEDIN_BLUE = Color(0xff0077b5);
+  static const Color BUTTON_WHITE = Color(0xfff0f0f0);
+  static const Color FONT_BLACK = Color(0xff212121);
+  static const Color ONBOARDING_GRAY = Color(0xff757575);
+  static const Color INPUT_WHITE = Color(0xfff0f0f0);
+}
