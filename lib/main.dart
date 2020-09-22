cc07958d2bcd2299aef92e05b392f53973355bae
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-23 01:00:13 +0300

    added firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Text(""),
+      home: Text("data"),
     );
   }
 }
