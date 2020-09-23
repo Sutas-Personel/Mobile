33f8e7e1bf85d05f8ec8a3c8faa354b3e00be371
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-23 07:58:06 +0300

    splash edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:SutasPersonel/core/constants/app_constants.dart';
 import 'package:SutasPersonel/core/init/lang/language_manager.dart';
+import 'package:SutasPersonel/view/splash/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
---
+++ lib/view/splash/splash_screen/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './splash_screen_view.dart';
+
+class SplashScreen extends StatefulWidget {
+  
+  @override
+  SplashScreenView createState() => new SplashScreenView();
+}
+  
