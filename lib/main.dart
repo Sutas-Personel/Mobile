cc0e0acd94b0f655b4c026c907b536f70a158d14
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-23 18:38:32 +0300

    edit pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:SutasPersonel/core/constants/app_constants.dart';
 import 'package:SutasPersonel/core/init/lang/language_manager.dart';
+import 'package:SutasPersonel/view/login_screen/login_screen.dart';
 
 import 'package:SutasPersonel/view/notifications_screen/notifications.dart';
 import 'package:SutasPersonel/view/profil_screen/profil.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-import 'view/splash_screen/splash_screen/splash_screen.dart';
+import 'view/notifications_screen/notifications.dart';
 
 void main() => runApp(EasyLocalization(
     path: ApplicationConstants.LANGUAGE_ASSET_PATH,
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: ProfilScreen(),
+      home: LoginScreen(),
     );
   }
 }
