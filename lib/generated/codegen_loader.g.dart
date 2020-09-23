85093c268e3126f6cc2af6749750401cf79b233d
fatihemree <fatih.emre.212@gmail.com>
2020-09-23 09:59:40 +0300

    language add

---
+++ lib/generated/codegen_loader.g.dart
@@ -0,0 +1,38 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> en = {
+  "routeFailed": "Bir sorun oluştu",
+  "profile": {
+    "Profile": "Profil",
+    "ProfilSettings": "Profil Settings",
+    "Language": "Language",
+    "Theme": "Theme",
+    "Exit": "Exit"
+  }
+};
+static const Map<String,dynamic> tr = {
+  "Bir sorunoluştu!": "routeFailed",
+  "profile": {
+    "Profile": "Profil",
+    "ProfilSettings": "Profil Ayarları",
+    "Language": "Dil",
+    "Theme": "Tema",
+    "Exit": "Çıkış Yap"
+  }
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en": en, "tr": tr};
+}
---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,12 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const routeFailed = 'routeFailed';
+  static const profile_Profile = 'profile.Profile';
+  static const profile_ProfilSettings = 'profile.ProfilSettings';
+  static const profile_Language = 'profile.Language';
+  static const profile_Theme = 'profile.Theme';
+  static const profile_Exit = 'profile.Exit';
+  static const profile = 'profile';
+
+}
