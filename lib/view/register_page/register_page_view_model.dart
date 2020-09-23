aaad87cca00dfb8ac28731f7ec6c014c04350c53
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-23 21:29:35 +0300

    register page finish

---
+++ lib/view/register_page/register_page_view_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './register_page.dart';
+
+abstract class RegisterPageViewModel extends State<RegisterPage> {
+  // Add your state and logic here
+
+  String nameFunc(String string) {}
+
+  String emailFunc(String string) {}
+
+  String passFunc(String string) {}
+
+  onPressLogIn() {}
+  registerOnpress() {}
+}
