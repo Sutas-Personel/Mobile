7228419a7ab81233b246e98ba78a05d9cd988bb8
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-27 01:58:38 +0300

    edit splash

--- lib/view/splash_screen/splash_screen_view_model.dart
+++ lib/view/splash_screen/splash_screen_view_model.dart
@@ -3,4 +3,16 @@ import './splash_screen.dart';
 
 abstract class SplashScreenViewModel extends State<SplashScreen> {
   // Add your state and logic here
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        Duration(
+          seconds: 3,
+        ), () {
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+    });
+  }
 }
