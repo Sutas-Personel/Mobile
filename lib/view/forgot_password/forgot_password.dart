74849c2e778b20bb1bd2a71384d6dbb307272bc8
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-26 17:25:59 +0300

    forgot password finish

---
+++ lib/view/forgot_password/forgot_password.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './forgot_password_view.dart';
+
+class ForgotPassword extends StatefulWidget {
+  
+  @override
+  ForgotPasswordView createState() => new ForgotPasswordView();
+}
+  
