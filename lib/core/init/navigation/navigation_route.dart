c6a6d233f5c0eb6b3c7e74330cf3e6804364db5c
fatihemree <fatih.emre.212@gmail.com>
2020-09-29 18:50:53 +0300

    comment navigationPush

--- lib/core/init/navigation/navigation_route.dart
+++ lib/core/init/navigation/navigation_route.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 
@@ -66,3 +66,4 @@ class NavigationRoute {
     );
   }
 }
+ */
