e6fbe875a2d92b9f75afdc482aa45b8db75f60c2
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-09-29 22:04:44 +0300

    added provider widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,25 @@
-import 'package:SutasPersonel/core/constants/app_constants.dart';
-import 'package:SutasPersonel/core/init/lang/language_manager.dart';
-import 'package:SutasPersonel/core/init/routes/router.dart';
-import 'package:SutasPersonel/view/bottom_navigation/bottom_navigation.dart';
-import 'package:SutasPersonel/view/forgot_password/forgot_password.dart';
-import 'package:SutasPersonel/view/home_screen/home_screen.dart';
-import 'package:SutasPersonel/view/login_screen/login_screen.dart';
-import 'package:SutasPersonel/view/notifications_screen/notifications.dart';
-import 'package:SutasPersonel/view/onboarding_screen/onboarding_view.dart';
-import 'package:SutasPersonel/view/operations_screen/operations.dart';
-import 'package:SutasPersonel/view/operations_screen/operations_view.dart';
-import 'package:SutasPersonel/view/profil_screen/profil.dart';
-import 'package:SutasPersonel/view/register_page/register_page.dart';
-import 'package:SutasPersonel/view/splash_screen/splash_screen.dart';
-import 'package:SutasPersonel/view/story_screen/story_screen.dart';
+import 'package:SutasPersonel/core/init/cache/locale_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
 
 import 'core/constants/app_constants.dart';
 import 'core/init/lang/language_manager.dart';
-import 'view/card_detail_screen/cardDetail.dart';
-import 'view/home_screen/home_screen.dart';
+import 'core/init/notifier/provider_list.dart';
+import 'core/init/notifier/theme_notifier.dart';
+import 'view/bottom_navigation/bottom_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(EasyLocalization(
-      path: ApplicationConstants.LANGUAGE_ASSET_PATH,
-      supportedLocales: LanguageManager.instance.supportedLocales,
-      child: MyApp()));
+  LocaleManager.prefrencesInit();
+  runApp(MultiProvider(
+    providers: [...ApplicationProvider.instance.dependItems],
+    child: EasyLocalization(
+        path: ApplicationConstants.LANGUAGE_ASSET_PATH,
+        supportedLocales: LanguageManager.instance.supportedLocales,
+        child: MyApp()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -35,11 +27,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      title: 'Material App',
-      home: BottomNavigation(),
-    );
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        theme: Provider.of<ThemeNotifier>(
+          context,
+          listen: true,
+        ).currentTheme,
+        locale: context.locale,
+        title: 'Material App',
+        home: BottomNavigation());
   }
 }
