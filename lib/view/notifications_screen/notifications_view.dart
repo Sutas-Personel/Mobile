b8dcbf99b95b542d0abac0ecc548b689b1cac549
fatihemree <fatih.emre.212@gmail.com>
2020-09-29 20:33:22 +0300

    notifications get data print ui

--- lib/view/notifications_screen/notifications_view.dart
+++ lib/view/notifications_screen/notifications_view.dart
@@ -1,11 +1,13 @@
 import 'package:SutasPersonel/core/components/list_tile_card.dart';
 import 'package:SutasPersonel/core/constants/colors.dart';
 import 'package:SutasPersonel/generated/locale_keys.g.dart';
+import 'package:SutasPersonel/models/notification_model.dart';
+import 'package:SutasPersonel/services/notifications_services.dart';
 import 'package:flutter/material.dart';
 import '../../core/extension/context_entension.dart';
 import 'notifications_view_model.dart';
 import '../../core/extension/string_extension.dart';
-
+import '../../core/extension/future_builder.dart';
 
 class NotificationsScreenView extends NotificationsScreenViewModel {
   @override
@@ -22,34 +24,41 @@ class NotificationsScreenView extends NotificationsScreenViewModel {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => Divider(),
-        itemCount: notifications.length,
-        itemBuilder: (context, index) => Dismissible(
-          key: UniqueKey(),
-          child: ListTileCard(
-            message: notifications[index],
-          ),
-          onDismissed: (direction) {
-            setState(() {
-              notifications.removeAt(index);
-            });
-          },
-          secondaryBackground: Container(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: context.lowValue),
-                child: Icon(Icons.cancel, color: Colors.white),
-              ),
-              color: context.colors.error),
-          background: Container(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: context.lowValue),
-                child: Icon(Icons.cancel, color: Colors.white),
-              ),
-              color: context.colors.error),
+      body: Future.value(NotificationService.instance.getNotificationList())
+          .toBuild<Object>(onSuccess: (data) {
+        return notificationsList(data);
+      }),
+    );
+  }
+
+  ListView notificationsList(List<NotificationsModel> notificationsList) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => Divider(),
+      itemCount: notificationsList.length,
+      itemBuilder: (context, index) => Dismissible(
+        key: UniqueKey(),
+        child: ListTileCard(
+          message: notificationsList[index],
         ),
+        onDismissed: (direction) {
+          setState(() {
+            notificationsList.removeAt(index);
+          });
+        },
+        secondaryBackground: Container(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: context.lowValue),
+              child: Icon(Icons.cancel, color: Colors.white),
+            ),
+            color: context.colors.error),
+        background: Container(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: context.lowValue),
+              child: Icon(Icons.cancel, color: Colors.white),
+            ),
+            color: context.colors.error),
       ),
     );
   }
--- lib/view/notifications_screen/notifications_view_model.dart
+++ lib/view/notifications_screen/notifications_view_model.dart
@@ -1,8 +1,27 @@
 import 'package:SutasPersonel/models/notification_model.dart';
+import 'package:SutasPersonel/services/interfaces/INotificationServices.dart';
+import 'package:SutasPersonel/services/notifications_services.dart';
 import 'package:flutter/material.dart';
-
 import 'notifications.dart';
 
 abstract class NotificationsScreenViewModel extends State<NotificationsScreen> {
-  List<NotificationsModel> notifications;
+  INotificationService notificationService;
+  List<NotificationsModel> getNotificationsList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notificationService = NotificationService.instance;
+    // getNotificationsLists();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  /* Future<void> getNotificationsLists() async {
+    getNotificationsList = await notificationService.getNotificationList();
+  } */
 }
